04109160852d3438829e3bd4d03981488e28602b
A O <alex.oshchepkov@gmail.com>
2018-02-16 21:20:58 +0500

    fix big files compilation

--- test/ts/dep.ts
+++ test/ts/dep.ts
@@ -1,3 +1,4 @@
+
 export const fn = () => {  
   console.log('function from dep module here')
 }
--- test/ts/test-script.ts
+++ test/ts/test-script.ts
@@ -1,4 +1,5 @@
 import {fn} from './dep'
+
 const str: string = process.argv[2]
 console.log('test', str)
 fn()
