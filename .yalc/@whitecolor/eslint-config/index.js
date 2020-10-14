a46212f309d0aa52a353419f07a3a2683c5e624d
A O <alex.oshchepkov@gmail.com>
2020-10-14 12:49:42 +0500

    eslint config

--- .yalc/@whitecolor/eslint-config/index.js
+++ .yalc/@whitecolor/eslint-config/index.js
@@ -1,40 +1,56 @@
 // eslint-disable-next-line no-undef
+const illegalGlobals = ['localStorage', 'name']
+const testGlobals = ['describe', 'it', 'context']
+
 module.exports = {
   env: {
     browser: true,
     es2020: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      files: ['*.test.*'],
+      rules: {
+        'no-restricted-globals': ['warn', ...illegalGlobals],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ["@typescript-eslint", "only-warn"],
+  plugins: ['@typescript-eslint', 'only-warn'],
   rules: {
-    "no-restricted-globals": ["warn", "localStorage"],
-    "no-restricted-properties": [
-      "warn",
+    'no-restricted-globals': ['warn', ...illegalGlobals, ...testGlobals],
+    'no-restricted-properties': [
+      'warn',
       {
-        object: "window",
-        property: "localStorage",
+        object: 'window',
+        property: 'localStorage',
       },
     ],
-    "no-console": ["warn", { allow: ["warn", "error"] }],
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
 
-    "@typescript-eslint/no-extra-semi": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-floating-promises": "warn",
-    "@typescript-eslint/no-misused-promises": "warn",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-extra-semi': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'warn',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
-};
+}
