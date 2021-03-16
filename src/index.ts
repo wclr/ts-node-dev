2ac49be8ed2c38335dae58e277080e6b9cd18753
Raphael Luba <raphael@leanbyte.com>
2021-03-16 13:20:12 +0100

    Fix: Don’t disconnect from the child if we kill it
    … otherwise the kill signal will not be received by the child reliably.

--- src/index.ts
+++ src/index.ts
@@ -220,9 +220,10 @@ export const runDev = (
     child.stopping = true
     child.respawn = true
     if (child.connected === undefined || child.connected === true) {
-      log.debug('Disconnecting from child')
-      child.disconnect()
-      if (!willTerminate) {
+      if (willTerminate) {
+        log.debug('Disconnecting from child')
+		child.disconnect()
+      } else {
         killChild()
       }
     }
